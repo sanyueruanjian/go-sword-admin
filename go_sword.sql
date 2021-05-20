558063560019e349e0c54919d0284932479dc7b6
linbolun <1668186101@qq.com>
2021-05-20 17:46:41 +0800

    feat:add go sword sql file

---
+++ go_sword.sql
@@ -0,0 +1,20 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : 120.26.141.144
+ Source Server Type    : MySQL
+ Source Server Version : 50730
+ Source Host           : 120.26.141.144:3306
+ Source Schema         : go_sword
+
+ Target Server Type    : MySQL
+ Target Server Version : 50730
+ File Encoding         : 65001
+
+ Date: 20/05/2021 12:15:00
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+SET FOREIGN_KEY_CHECKS = 1;
